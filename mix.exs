74a91643a529b86a7434c5fdfe631b424b26a420
Johann Og <kronolynx@gmail.com>
2016-08-09 15:51:24 +0200

    Update readme, add example

--- mix.exs
+++ mix.exs
@@ -7,36 +7,32 @@ defmodule Stampery.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
-    [
-        mod: {Example, []},
-        applications: [:logger, :sha3, :msgpack_rpc, :amqp]
-        # applications: [:logger, :amqp]
-    ]
+    [applications: [:logger, :sha3, :msgpack_rpc, :amqp]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [
         {:amqp, "~> 0.1.4"},
         {:sha3, "~> 1.0.0", override: true, compile: "make"},
-        # msgpackrpc was overriding proper in msgpack so I had to include proper here to fix it
+        # msgpackrpc was overriding msgpack proper so I had to override it again to disambiguate
         {:proper, ~r/.*/, [env: :prod, git: "git://github.com/manopapad/proper.git", branch: "master", manager: :rebar, override: true]},
         {:msgpack_rpc, github: "stampery/msgpack-rpc-erlang", tag: "fix/latest-otp"}
     ]
+  end
+
+  defp description do
+    """
+    Stampery API for Elixir. Notarize all your data using the blockchain! 
+    """
+  end
+
+  defp package do
+    [maintainer: ["Johann Ortiz"],
+     links: %{"Stampery" => "https://stampery.com",
+              "GitHub" => "https://github.com/stampery/elixir"}]
   end
 end
